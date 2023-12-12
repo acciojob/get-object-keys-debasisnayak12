13c6f12f252ffcd7abaec3321bc3fac986e6a29a
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-12 17:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 const student = {
-	name : "Debasis",
-}
+	name : "Debasis"
+};
 
-Object.prototype.getKeys(){
+Object.prototype.getKeys = function () {
 	return Object.keys(this);
-}
+};
 
 console.log(student.getKeys());
